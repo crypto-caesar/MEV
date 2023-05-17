9d1145ffa579db4da82974cf061bdb14eb845620
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-05-17 09:30:30 -0400

    Create tick_lens.py

---
+++ tick_lens.py
@@ -0,0 +1,28 @@
+''' 
+to retrieve tick info for the main v3_liquidity_pool helper, we need to build a
+helper to query the TickLens contract. 
+'''
+
+
+from abc import ABC, abstractmethod
+
+from brownie import Contract
+from brownie.convert import to_address
+
+
+class TickLens(ABC):
+    def __init__(
+        self,
+        address="0xbfd8137f7d1516D3ea5cA83523914859ec47F573"
+    ):
+        self.address = to_address(address)
+
+        try:
+            self._brownie_contract = Contract(address=address)
+        except:
+            try:
+                self._brownie_contract = Contract.from_explorer(
+                    address=address, silent=True
+                )
+            except:
+                raise
