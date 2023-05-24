26bcbcfcd3cae15652e604c5d56cef8b9f70c204
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-05-24 08:54:22 -0400

    Create chainlink.py

---
+++ chainlink.py
@@ -0,0 +1,31 @@
+from brownie import Contract  # type: ignore
+
+
+class ChainlinkPriceContract:
+    """
+    Represents an on-chain Chainlink price oracle.
+    Variable 'price' is decimal-corrected and represents the "nominal" token price in USD (e.g. 1 DAI = 1.0 USD)
+    """
+
+    def __init__(
+        self,
+        address: str,
+    ) -> None:
+        try:
+            self._contract = Contract(address)
+        except Exception as e:
+            print(e)
+            try:
+                self._contract = Contract.from_explorer(address)
+            except Exception as e:
+                print(e)
+
+        self._decimals: int = self._contract.decimals()
+        self.update_price()
+
+    def update_price(
+        self,
+    ) -> None:
+        self.price: float = self._contract.latestRoundData()[1] / (
+            10**self._decimals
+        )
