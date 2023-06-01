899cc4f9fb14c9214497e3ce8731b05809fc5b56
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-06-01 09:20:36 -0400

    Update flash_borrow_to_lp_swap_new.py

--- arbitrage/flash_borrow_to_lp_swap_new.py
+++ arbitrage/flash_borrow_to_lp_swap_new.py
@@ -4,8 +4,8 @@ from typing import List, Optional, Tuple
 from brownie import Contract  # type: ignore
 from scipy.optimize import minimize_scalar  # type: ignore
 
-from MEV.uniswap.v2.liquidity_pool import LiquidityPool
-from MEV.token import Erc20Token
+from alex_bot.uniswap.v2.liquidity_pool import LiquidityPool
+from alex_bot.token import Erc20Token
 
 # TODO: improve arbitrage calculation for repaying with same token, instead of borrow A -> repay B
 
