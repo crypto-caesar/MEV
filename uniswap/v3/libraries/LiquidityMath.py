095b449a479ffa2f8499751973cd69c9f4fd1c14
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-06-01 09:01:41 -0400

    Create LiquidityMath.py

---
+++ uniswap/v3/libraries/LiquidityMath.py
@@ -0,0 +1,31 @@
+from alex_bot.exceptions import EVMRevertError
+from .Helpers import *
+
+
+def addDelta(x: int, y: int) -> int:
+    """
+    This function has been heavily modified to directly check that the result fits in a uint128,
+    instead of checking via < or >= tricks via Solidity's built-in casting as implemented at
+    https://github.com/Uniswap/v3-core/blob/main/contracts/libraries/LiquidityMath.sol
+    """
+
+    if not (0 <= x <= 2**128 - 1):
+        raise EVMRevertError("x not a valid uint128")
+    # assert 0 <= x <= 2**128 - 1, "x not a valid uint128"
+
+    if not (-(2**127) <= y <= 2**127 - 1):
+        raise EVMRevertError("y not a valid int128")
+    # assert -(2**127) <= y <= 2**127 - 1, "y not a valid int128"
+
+    if y < 0:
+        z = x - uint128(-y)
+        if not (0 <= z <= 2**128 - 1):
+            raise EVMRevertError("LS")
+        # assert 0 <= z <= 2**128 - 1, "LS"
+    else:
+        z = x + uint128(y)
+        if not (0 <= z <= 2**128 - 1):
+            raise EVMRevertError("LA")
+        # assert 0 <= z <= 2**128 - 1, "LA"
+
+    return z
