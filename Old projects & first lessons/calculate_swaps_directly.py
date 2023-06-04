7aac3a107b7113434262d31b14b9a734dbe57fbf
crypto-caesar <113930650+crypto-caesar@users.noreply.github.com>
2023-06-04 08:11:28 -0400

    Update and rename calculate_swaps_directly_out_for_in.py to calculate_swaps_directly.py

--- Old projects & first lessons/calculate_swaps_directly.py
+++ Old projects & first lessons/calculate_swaps_directly.py
@@ -37,6 +37,40 @@ def get_tokens_out_from_tokens_in(
             pool_reserves_token1 + quantity_token1_in * (1 - fee)
         )
 
+def get_tokens_in_for_ratio_out(
+    pool_reserves_token0,
+    pool_reserves_token1,
+    token0_per_token1,
+    token0_out=False,
+    token1_out=False,
+    fee=Decimal("0.0"),
+):
+    assert not (token0_out and token1_out)
+
+    # token1 input, token0 output
+    if token0_out:
+        # dy = x0/C - y0/(1-fee)
+        # C = ratio of token0 (dx) to token1 (dy)
+        dy = int(
+            pool_reserves_token0 / token0_per_token1 - pool_reserves_token1 / (1 - fee)
+        )
+        if dy > 0:
+            return dy
+        else:
+            return 0
+
+    # token0 input, token1 output
+    if token1_out:
+        # dx = y0*C - x0/(1-fee)
+        # C = ratio of token0 (dxy) to token1 (dy)
+        dx = int(
+            pool_reserves_token1 * token0_per_token1 - pool_reserves_token0 / (1 - fee)
+        )
+        if dx > 0:
+            return dx
+        else:
+            return 0
+
 def contract_load(address, alias):
     # Attempts to load the saved contract by alias.
     # If not found, fetch from network explorer and set alias.
